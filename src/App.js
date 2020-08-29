d2db1c1457e7f252045fdc6fee41c22798a0562a
devabu23 <devabu23>
2020-08-30 02:58:19 +0600

    Breaking News-Website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import News from './components/News/News';
+import Header from './components/Header/Header';
 
 function App() {
+  const [articles, setArticle] = useState([]);
+  useEffect(() => {
+    fetch('http://newsapi.org/v2/top-headlines?country=us&apiKey=182c7fb87c024effa687e65a5e84f937')
+    .then(res => res.json())
+    .then(data => setArticle(data.articles))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      {
+        articles.map(article => <News article={article}></News>)
+      }
+    </>
   );
 }
 
